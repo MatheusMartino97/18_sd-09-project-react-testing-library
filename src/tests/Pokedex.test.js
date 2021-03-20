3062494f8ceac11620de6d09e949d534c7d0dfdf
Matheus Martino <matheus.martino97@gmail.com>
2021-03-20 03:59:11 -0300

    Beggining te tests for Pokedex.

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,8 @@
-test('', () => {});
+import React from 'react';
+import renderWithRouter from '../helpers/renderWithRouter';
+import Pokedex from '../components/Pokedex';
+
+describe('Testando o componente Pokedex', () => {
+  it('Teste se página contém um heading h2 com o texto Encountered pokémons', () => {
+  });
+});
