ed7cc7636fadf0caa2697582b0f7c348b2f1afbd
Matheus Martino <matheus.martino97@gmail.com>
2021-04-13 23:29:30 -0300

    Updates

---
+++ precache-manifest.b1a1fdbb79fc66fcdfa869b386223c94.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6bdfcff76b1c23697d0d7e3ec3f8b05",
+    "url": "/18_sd-09-project-react-testing-library/index.html"
+  },
+  {
+    "revision": "0dc311a53748099699dc",
+    "url": "/18_sd-09-project-react-testing-library/static/css/main.88a5c871.chunk.css"
+  },
+  {
+    "revision": "760c68df0d5e85e1e9f9",
+    "url": "/18_sd-09-project-react-testing-library/static/js/2.01afe734.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/18_sd-09-project-react-testing-library/static/js/2.01afe734.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0dc311a53748099699dc",
+    "url": "/18_sd-09-project-react-testing-library/static/js/main.b9340dbe.chunk.js"
+  },
+  {
+    "revision": "699328eeb41ff34c468e",
+    "url": "/18_sd-09-project-react-testing-library/static/js/runtime-main.9b57e481.js"
+  }
+]);
